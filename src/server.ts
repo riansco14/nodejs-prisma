39a713c8f2c11db5b2001072b6fc41038947d7bc
Rian <rian.azevedo@qintess.com>
2023-03-27 23:09:16 -0300

    create: configs

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('Teste')
+import { app } from './app'
+import { env } from './env'
+
+app
+	.listen({
+		port: env.PORT,
+	})
+	.then(() => {
+		console.log('Servidor Iniciado, Porta 3333 ')
+	})
